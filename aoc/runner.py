48b8b27b0174eccb889b0708b5c8170a9101c954
Moritz Strenger <moritz.strenger@gmail.com>
2022-12-03 10:19:33 +0100

    :bug: Fix edge case

--- aoc/runner.py
+++ aoc/runner.py
@@ -18,10 +18,12 @@ def _parse_task(day, part):
         logger.info(f"Defaulting to {day=}")
 
     if part not in (1, 2):
-        if (_TASK_ROOT / f"day_{day:02d}" / f"part_1.py").exists():
+        if _TASK_ROOT.joinpath(f"day_{day:02d}").joinpath(f"part_2.py").exists():
             part = 2
-        else:
+        elif _TASK_ROOT.joinpath(f"day_{day:02d}").joinpath(f"part_1.py").exists():
             part = 1
+        else:
+            part = -1
         logger.info(f"Defaulting to {part=}")
 
     return day, part
@@ -39,17 +41,17 @@ def run(day: int = -1, part: int = -1) -> None:
 def generate(day: int = -1, part: int = -1) -> None:
     day, part = _parse_task(day, part)
 
-    if part == 1:
+    if part < 1:
         try:
             shutil.copytree(_TEMPLATE_PATH, _TASK_ROOT / f"day_{day:02d}")
         except FileExistsError:
             logger.warning("Already created Python code.")
 
-        path = DATA_ROOT / f"day_{day:02d}" / f"part_{part}"
+        path = DATA_ROOT / f"day_{day:02d}" / f"part_1"
         path.mkdir(parents=True)
         with open(path / "input.txt", "w+"):
             pass
-    elif part == 2:
+    elif part == 1:
         try:
             shutil.copy(_TASK_ROOT / f"day_{day:02d}" / f"part_1.py", _TASK_ROOT / f"day_{day:02d}" / f"part_2.py")
         except FileExistsError:
