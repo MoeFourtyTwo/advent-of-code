13a457b25c15b5e1c17e85977b66d2f3ad034c2b
Moritz Strenger <moritz.strenger@gmail.com>
2022-12-06 20:30:20 +0100

    :sparkles: Solve day 6

---
+++ aoc/tasks/day_06/part_1.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go():
+    [line] = get_lines(DATA_PATH)
+
+    index = 0
+    l = 4
+    for index in range(l, len(line)):
+        s = set(line[index - l : index])
+        if len(s) == l:
+            break
+
+    logger.info(f"{index=}")
