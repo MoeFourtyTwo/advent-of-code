618f11810e9a84d10abf180d2e12ee8b1902af31
Moritz Strenger <moritz.strenger@gmail.com>
2022-12-24 18:21:14 +0100

    :zap: Improve day 24

--- aoc/tasks/day_24/part_2.py
+++ aoc/tasks/day_24/part_2.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 
-import dataclasses
 import enum
-import functools
-import heapq
-import math
+import itertools
 import pathlib
 import typing
 
-import tqdm
 from loguru import logger
 
 from aoc.common.decorators import timeit
@@ -37,27 +33,20 @@ class Direction(complex, enum.Enum):
             case "^":
                 return Direction.up
 
-    def to_char(self) -> str:
-        match self:
-            case Direction.right:
-                return ">"
-            case Direction.down:
-                return "v"
-            case Direction.left:
-                return "<"
-            case Direction.up:
-                return "^"
 
-
-@dataclasses.dataclass(frozen=True)
 class Field:
-    clock: int
-    width: int
-    height: int
-    start: complex
-    target: complex
+    def __init__(self, clock: int, width: int, height: int, start: complex, target: complex, blizzards: list[BLIZZARD]):
+        self.clock = clock
+        self.width = width
+        self.height = height
+        self.start = start
+        self.target = target
+        self.blizzards = blizzards
+        self.elves = {self.start}
 
-    blizzards: list[BLIZZARD]
+    def flip(self):
+        self.start, self.target = self.target, self.start
+        self.elves = {self.start}
 
     @classmethod
     def parse_input(cls, lines: list[str]) -> Field:
@@ -78,10 +67,19 @@ class Field:
             ],
         )
 
-    def next_field(self) -> Field:
-        return dataclasses.replace(
-            self, clock=self.clock + 1, blizzards=[self.update_blizzard(blizzard) for blizzard in self.blizzards]
-        )
+    def solve(self) -> int:
+        while self.target not in self.elves:
+            self.next_field()
+            self.iterate()
+            self.clock += 1
+        return self.clock
+
+    def iterate(self) -> None:
+        next_elves = set(itertools.chain.from_iterable(self.generate_options(elf) for elf in self.elves))
+        self.elves = set(next_elves) - set(blizzard[0] for blizzard in self.blizzards)
+
+    def next_field(self) -> None:
+        self.blizzards = [self.update_blizzard(blizzard) for blizzard in self.blizzards]
 
     def update_blizzard(self, blizzard: BLIZZARD) -> BLIZZARD:
         position, direction = blizzard
@@ -105,121 +103,46 @@ class Field:
                     return new_position + complex(0, self.height - 2), direction
                 return new_position, direction
 
-    @functools.cached_property
-    def data(self) -> frozenset[complex]:
-        return frozenset(blizzard[0] for blizzard in self.blizzards)
-
-    def generate_options(self, option: Option) -> list[Option]:
-        options = []
-
-        if option.position not in self.data:
-            options.append(option.stay())
-
-        for direction in Direction:
-            if (new_position := option + direction).in_bounds(self) and new_position.position not in self.data:
-                options.append(new_position)
-
-        for direction in [Direction.down, Direction.up]:
-            if (new_position := option + direction).finished:
-                options = [option for option in options if not option.finished]
-                updated_option = new_position.flip()
-                options.append(updated_option)
-
-                if updated_option.fully_finished:
-                    return [updated_option]
-
-        return options
-
-
-@dataclasses.dataclass(frozen=True, eq=True)
-class Option:
-    start: complex
-    target: complex
-    position: complex
-    clock: int
-    finish_counter: int = 0
-
-    @property
-    def priority(self) -> tuple[int, int, int]:
-        return (
-            -self.finish_counter,
-            self.clock,
-        )
-
-    def __lt__(self, other: Option) -> bool:
-        return self.priority < other.priority
-
-    def __add__(self, other: Direction) -> Option:
-        return dataclasses.replace(self, position=self.position + other, clock=self.clock + 1)
-
-    def stay(self) -> Option:
-        return dataclasses.replace(self, clock=self.clock + 1)
-
-    def flip(self) -> Option:
-        return dataclasses.replace(
-            self,
-            start=self.target,
-            target=self.start,
-            finish_counter=self.finish_counter + 1,
-        )
-
-    def in_bounds(self, field: Field) -> bool:
-        if self.position == field.start or self.position == field.target:
+    def in_bounds(self, position: complex) -> bool:
+        if position == self.start or position == self.target:
             return True
 
-        return 0 < int(self.position.real) < field.width - 1 and 0 < int(self.position.imag) < field.height - 1
+        return 0 < int(position.real) < self.width - 1 and 0 < int(position.imag) < self.height - 1
 
-    @property
-    def finished(self) -> bool:
-        return self.position == self.target
+    def generate_options(self, position: complex) -> list[complex]:
+        options = [position]
 
-    @property
-    def fully_finished(self) -> bool:
-        return self.finish_counter == 3
+        for direction in Direction:
+            if self.in_bounds(new_position := position + direction):
+                options.append(new_position)
 
-
-def field_manager(initial_field: Field) -> typing.Callable[[int], Field]:
-    fields = {0: initial_field}
-
-    def inner(n: int) -> Field:
-        if n in fields:
-            return fields[n]
-
-        for i in range(max(fields.keys()) + 1, n + 1):
-            fields[i] = fields[i - 1].next_field()
-
-        return fields[n]
-
-    return inner
+        return options
 
 
 @timeit
 def go(path: pathlib.Path = DATA_PATH):
     lines = get_lines(path)
 
-    field_getter = field_manager(Field.parse_input(lines))
+    field = Field.parse_input(lines)
 
-    heap = []
-    start_option = Option(field_getter(0).start, field_getter(0).target, field_getter(0).start, 0)
-    heapq.heappush(heap, start_option)
-    known_states = {start_option}
-    with tqdm.tqdm() as pbar:
-        while heap:
-            pbar.update()
-            option: Option = heapq.heappop(heap)
+    min_time = field.solve()
 
-            current_field = field_getter(option.clock + 1)
-            next_options = current_field.generate_options(option)
-            for next_option in next_options:
-                if next_option not in known_states:
-                    if next_option.finished:
-                        known_states = {next_option}
-                        heap = []
-                        heapq.heappush(heap, next_option)
-                        break
-                    if next_option.fully_finished:
-                        logger.info(f"{next_option.clock=}")
-                        return next_option.clock
-                    else:
-                        known_states.add(next_option)
-                        heapq.heappush(heap, next_option)
+    logger.info(f"{min_time=}")
+
+    return min_time
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH):
+    lines = get_lines(path)
+
+    field = Field.parse_input(lines)
+
+    field.solve()
+    field.flip()
+    field.solve()
+    field.flip()
+    min_time = field.solve()
+
+    logger.info(f"{min_time=}")
+    return min_time
