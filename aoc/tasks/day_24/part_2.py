9733efbb2d33810c50c7fc82535776cbaccb5d2f
Moritz Strenger <moritz.strenger@gmail.com>
2022-12-25 23:40:28 +0100

    :zap: Improve day 24

--- aoc/tasks/day_24/part_2.py
+++ aoc/tasks/day_24/part_2.py
@@ -1,148 +1,118 @@
 from __future__ import annotations
 
 import enum
+import functools
 import itertools
 import pathlib
 import typing
 
+import numpy as np
 from loguru import logger
+from scipy.ndimage import convolve
 
 from aoc.common.decorators import timeit
 from aoc.common.storage import get_data_path, get_lines
 
 DATA_PATH = get_data_path(__file__)
-
-BLIZZARD: typing.TypeAlias = tuple[complex, complex]
-
-
-class Direction(complex, enum.Enum):
-    right = 1 + 0j
-    down = 1j
-    left = -1 + 0j
-    up = -1j
-
-    @classmethod
-    def from_char(cls, char: str) -> Direction:
-        match char:
-            case ">":
-                return Direction.right
-            case "v":
-                return Direction.down
-            case "<":
-                return Direction.left
-            case "^":
-                return Direction.up
+BLIZZARDS: typing.TypeAlias = tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+ELF_KERNEL = [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+]
 
 
-class Field:
-    def __init__(self, clock: int, width: int, height: int, start: complex, target: complex, blizzards: list[BLIZZARD]):
-        self.clock = clock
-        self.width = width
-        self.height = height
-        self.start = start
-        self.target = target
-        self.blizzards = blizzards
-        self.elves = {self.start}
+class Direction(enum.Enum):
+    right = enum.auto()
+    down = enum.auto()
+    left = enum.auto()
+    up = enum.auto()
 
-    def flip(self):
-        self.start, self.target = self.target, self.start
-        self.elves = {self.start}
+    def to_char(self) -> str:
+        match self:
+            case self.right:
+                return ">"
+            case self.down:
+                return "v"
+            case self.left:
+                return "<"
+            case self.up:
+                return "^"
 
-    @classmethod
-    def parse_input(cls, lines: list[str]) -> Field:
-        height = len(lines)
-        width = len(lines[0])
 
-        return Field(
-            clock=0,
-            width=width,
-            height=height,
-            start=1 + 0j,
-            target=complex(width - 2, height - 1),
-            blizzards=[
-                (complex(x, y), Direction.from_char(char))
-                for y, line in enumerate(lines)
-                for x, char in enumerate(line)
-                if char in ">v<^"
-            ],
+@timeit
+def parse_input(path: pathlib.Path) -> tuple[BLIZZARDS, np.ndarray]:
+    lines = get_lines(path)
+    # noinspection PyTypeChecker
+    blizzards: BLIZZARDS = tuple(parse_blizzard(lines, direction.to_char()) for direction in Direction)
+    walkable = np.array([[char != "#" for char in line] for line in lines])
+    return blizzards, walkable
+
+
+def parse_blizzard(lines: list[str], search_char: str) -> np.ndarray:
+    return np.array([[char != search_char for char in line[1:-1]] for line in lines[1:-1]])
+
+
+def generate_rotation_func(arrays: BLIZZARDS):
+    @functools.cache
+    def right(index: int) -> np.ndarray:
+        return np.roll(arrays[0], index, axis=1)
+
+    @functools.cache
+    def down(index: int) -> np.ndarray:
+        return np.roll(arrays[1], index, axis=0)
+
+    @functools.cache
+    def left(index: int) -> np.ndarray:
+        return np.roll(arrays[2], -index, axis=1)
+
+    @functools.cache
+    def up(index: int) -> np.ndarray:
+        return np.roll(arrays[3], -index, axis=0)
+
+    def rotate_wind(clock: int) -> BLIZZARDS:
+        return (
+            right(clock % arrays[0].shape[1]),
+            down(clock % arrays[0].shape[0]),
+            left(clock % arrays[0].shape[1]),
+            up(clock % arrays[0].shape[0]),
         )
 
-    def solve(self) -> int:
-        while self.target not in self.elves:
-            self.next_field()
-            self.iterate()
-            self.clock += 1
-        return self.clock
-
-    def iterate(self) -> None:
-        next_elves = set(itertools.chain.from_iterable(self.generate_options(elf) for elf in self.elves))
-        self.elves = set(next_elves) - set(blizzard[0] for blizzard in self.blizzards)
-
-    def next_field(self) -> None:
-        self.blizzards = [self.update_blizzard(blizzard) for blizzard in self.blizzards]
-
-    def update_blizzard(self, blizzard: BLIZZARD) -> BLIZZARD:
-        position, direction = blizzard
-        new_position = position + direction
-
-        match direction:
-            case Direction.right:
-                if new_position.real >= self.width - 1:
-                    return new_position - (self.width - 2), direction
-                return new_position, direction
-            case Direction.left:
-                if new_position.real < 1:
-                    return new_position + self.width - 2, direction
-                return new_position, direction
-            case Direction.down:
-                if new_position.imag >= self.height - 1:
-                    return new_position - complex(0, self.height - 2), direction
-                return new_position, direction
-            case Direction.up:
-                if new_position.imag < 1:
-                    return new_position + complex(0, self.height - 2), direction
-                return new_position, direction
-
-    def in_bounds(self, position: complex) -> bool:
-        if position == self.start or position == self.target:
-            return True
-
-        return 0 < int(position.real) < self.width - 1 and 0 < int(position.imag) < self.height - 1
-
-    def generate_options(self, position: complex) -> list[complex]:
-        options = [position]
-
-        for direction in Direction:
-            if self.in_bounds(new_position := position + direction):
-                options.append(new_position)
-
-        return options
+    return rotate_wind
 
 
 @timeit
-def go(path: pathlib.Path = DATA_PATH):
-    lines = get_lines(path)
+def solve(wind_func, blizzards: BLIZZARDS, walkable, start, target) -> tuple[BLIZZARDS, int]:
+    elves = np.zeros_like(walkable, dtype=bool)
+    elves[start] = True
+    minute = 0
 
-    field = Field.parse_input(lines)
+    while not elves[target]:
+        minute += 1
+        blizzards = wind_func(minute)
+        convolve(elves, ELF_KERNEL, output=elves, mode="constant", cval=False)
+        np.logical_and(elves, walkable, out=elves)
+        for blizzard in blizzards:
+            np.logical_and(elves[1:-1, 1:-1], blizzard, out=elves[1:-1, 1:-1])
 
-    min_time = field.solve()
-
-    logger.info(f"{min_time=}")
-
-    return min_time
+    return blizzards, minute
 
 
 @timeit
-def go(path: pathlib.Path = DATA_PATH):
-    lines = get_lines(path)
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    blizzards, walkable = parse_input(path)
 
-    field = Field.parse_input(lines)
+    wind_func = generate_rotation_func(blizzards)
 
-    field.solve()
-    field.flip()
-    field.solve()
-    field.flip()
-    min_time = field.solve()
+    blizzards, time_first_trip = solve(
+        wind_func, blizzards, walkable, (0, 1), (walkable.shape[0] - 1, walkable.shape[1] - 2)
+    )
+    blizzards, time_second_trip = solve(
+        wind_func, blizzards, walkable, (walkable.shape[0] - 1, walkable.shape[1] - 2), (0, 1)
+    )
+    _, time_third_trip = solve(wind_func, blizzards, walkable, (0, 1), (walkable.shape[0] - 1, walkable.shape[1] - 2))
 
-    logger.info(f"{min_time=}")
-    return min_time
+    total_time = time_first_trip + time_second_trip + time_third_trip
+
+    logger.info(f"{total_time}")
+    return total_time
