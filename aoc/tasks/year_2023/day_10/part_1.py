37f1d43b0eabe0f9cf3a4718c507dcf4f2daa2b6
Moritz Strenger <moritz.strenger@gmail.com>
2023-12-10 17:56:14 +0100

    :sparkles: Solve day 10 part 1

---
+++ aoc/tasks/year_2023/day_10/part_1.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import pathlib
+
+import numpy as np
+from loguru import logger
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    data = np.array([list(line) for line in lines])
+    data = np.pad(data, 1, constant_values=".")
+
+    ([row], [column]) = np.where(data == "S")
+
+    last_row, last_column = row, column
+    distance = 0
+
+    while data[row, column] != "S" or distance == 0:
+        current_pos_cache = (row, column)
+        distance += 1
+        match data[row, column]:
+            case "|":
+                row += row - last_row
+            case "-":
+                column += column - last_column
+            case "L":
+                if last_column == column:
+                    column += 1
+                else:
+                    row -= 1
+            case "J":
+                if last_column == column:
+                    column -= 1
+                else:
+                    row -= 1
+            case "7":
+                if last_column == column:
+                    column -= 1
+                else:
+                    row += 1
+            case "F":
+                if last_column == column:
+                    column += 1
+                else:
+                    row += 1
+            case "S":
+                if data[row + 1, column] in ("|", "L", "J"):
+                    row += 1
+                elif data[row, column + 1] in ("-", "J", "7"):
+                    column += 1
+                elif data[row - 1, column] in ("-", "L", "F"):
+                    row -= 1
+                else:
+                    column -= 1
+        last_row, last_column = current_pos_cache
+
+    return distance // 2
