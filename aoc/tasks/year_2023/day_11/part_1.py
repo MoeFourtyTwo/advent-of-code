fd034bde164330cf7403fb40c9ddf5284fa0ff8b
Moritz Strenger <moritz.strenger@gmail.com>
2023-12-11 21:01:15 +0100

    :sparkles: Solve day 11

---
+++ aoc/tasks/year_2023/day_11/part_1.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import itertools
+import pathlib
+
+import numpy as np
+from tqdm import tqdm
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+def calc_distance(source: tuple[int, int], target: tuple[int, int], empty_rows: set[int], empty_cols: set[int]) -> int:
+    row_range = range(min(source[0], target[0]), max(source[0], target[0]))
+    col_range = range(min(source[1], target[1]), max(source[1], target[1]))
+
+    empty_row_count = sum(empty_row in row_range for empty_row in empty_rows)
+    empty_col_count = sum(empty_col in col_range for empty_col in empty_cols)
+
+    distance = abs(source[1] - target[1]) + abs(source[0] - target[0]) + empty_col_count + empty_row_count
+
+    return distance
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    data = np.array([list(line) for line in lines])
+
+    empty_rows = set(np.where(np.all(data == ".", axis=1))[0])
+    empty_cols = set(np.where(np.all(data == ".", axis=0))[0])
+
+    positions = map(tuple, zip(*np.where(data == "#")))
+
+    all_pairs = list(itertools.combinations(positions, 2))
+
+    distance = sum(calc_distance(source, target, empty_rows, empty_cols) for source, target in tqdm(all_pairs))
+
+    return distance
---
+++ tests/tasks/year_2023/day_11/test_part_2.py
@@ -0,0 +1,8 @@
+from aoc.common.storage import get_data_path
+from aoc.tasks.year_2023.day_11.part_2 import go
+
+TEST_PATH = get_data_path(__file__, "test.txt")
+
+
+def test_year_2023_day_11_part_2_go():
+    assert True
