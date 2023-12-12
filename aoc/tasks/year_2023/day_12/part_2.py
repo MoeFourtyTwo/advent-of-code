8943ddb16bf4abff09007054cf7b0c473047c6f6
Moritz Strenger <moritz.strenger@gmail.com>
2023-12-12 23:23:51 +0100

    :sparkles: Solve day 12

---
+++ aoc/tasks/year_2023/day_12/part_2.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import functools
+import pathlib
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@functools.cache
+def count(line: str, groups: tuple[int, ...]) -> int:
+    if len(line) == 0:
+        return int(len(groups) == 0)
+
+    match line[0]:
+        case ".":
+            return count(line[1:], groups)
+        case "?":
+            return count(line[1:], groups) + count("#" + line[1:], groups)
+        case "#":
+            if len(groups) == 0:
+                return 0
+
+            if len(line) < groups[0]:
+                return 0
+
+            if "." in line[: groups[0]]:
+                return 0
+
+            if len(groups) > 1:
+                if len(line) < groups[0] + 1 or line[groups[0]] == "#":
+                    return 0
+                return count(line[groups[0] + 1 :], groups[1:])
+            else:
+                return count(line[groups[0] :], groups[1:])
+
+
+def parse_line(line: str) -> tuple[str, tuple[int, ...]]:
+    line, damage_groups = line.split()
+    return "?".join([line] * 5), tuple(map(int, ((damage_groups + ",") * 5)[:-1].split(",")))
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH):
+    lines = get_lines(path)
+
+    return sum(count(*parse_line(line)) for line in lines)
---
+++ tests/tasks/year_2023/day_12/test_part_2.py
@@ -0,0 +1,8 @@
+from aoc.common.storage import get_data_path
+from aoc.tasks.year_2023.day_12.part_2 import go
+
+TEST_PATH = get_data_path(__file__, "test.txt")
+
+
+def test_year_2023_day_12_part_2_go():
+    assert go(TEST_PATH) == 525152
