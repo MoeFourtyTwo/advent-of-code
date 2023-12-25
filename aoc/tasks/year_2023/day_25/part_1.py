aa2970d698d92158778e2c113ede5d2829ec09ff
Moritz Strenger <moritz.strenger@gmail.com>
2023-12-25 22:16:05 +0100

    Add Day 25, Part 1 solution for AoC 2023
    This commit includes a graph-based solution to Part 1 of Day 25 challenge for Advent of Code 2023, with adequate unit tests and accompanying test data. All these new codes reside in `part_1.py`, `test_part_1.py`, and `test.txt` respectively.

---
+++ aoc/tasks/year_2023/day_25/part_1.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import functools
+import itertools
+import operator
+import pathlib
+
+import networkx as nx
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+    g = nx.Graph()
+    for line in lines:
+        node, other_nodes = line.split(": ")
+        for other_node in other_nodes.split():
+            g.add_edge(node, other_node)
+
+    h = nx.algorithms.connectivity.build_auxiliary_edge_connectivity(g)
+    r = nx.algorithms.flow.build_residual_network(h, "capacity")
+
+    for u, v in itertools.combinations(g, 2):
+        cut = nx.algorithms.connectivity.minimum_st_edge_cut(g, u, v, auxiliary=h, residual=r)
+        if len(cut) != 3:
+            continue
+
+        for edge in cut:
+            g.remove_edge(*edge)
+
+        return functools.reduce(operator.mul, map(len, nx.connected_components(g)), 1)
+
+    raise ValueError(f"No solution found")
---
+++ tests/tasks/year_2023/day_25/test_part_1.py
@@ -0,0 +1,8 @@
+from aoc.common.storage import get_data_path
+from aoc.tasks.year_2023.day_25.part_1 import go
+
+TEST_PATH = get_data_path(__file__, "test.txt")
+
+
+def test_year_2023_day_25_part_1_go():
+    assert go(TEST_PATH) == 54
