693b0eebee25c2520d7ed8d5f667310501348755
Moritz Strenger <moritz.strenger@gmail.com>
2024-12-10 19:56:14 +0100

    Add solution and tests for 2024 Day 10 tasks.
    Implemented parts 1 and 2 for the Day 10 challenge, including helper methods and logic to compute reachable peaks in a grid. Added test cases and sample data to validate correctness, with expected outputs of 36 and 81 for parts 1 and 2 respectively.

---
+++ aoc/tasks/year_2024/day_10/part_2.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import pathlib
+from collections import defaultdict
+
+import numpy as np
+from numpy.typing import NDArray
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_as_array, get_data_path
+
+DATA_PATH = get_data_path(__file__)
+
+Point = tuple[int, int]
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    grid = get_as_array(path)
+
+    grid = np.pad(grid, pad_width=1, mode="constant", constant_values=-2)
+
+    score = 0
+
+    known = defaultdict(int)
+    for i in range(1, grid.shape[0] - 1):
+        for j in range(1, grid.shape[1] - 1):
+            find_reachable_peaks(grid, known, (i, j))
+
+            if grid[i, j] == 0:
+                score += known[(i, j)]
+
+    return score
+
+
+def find_reachable_peaks(grid: NDArray[Point, np.int_], known: dict[Point, int], position: Point) -> None:
+    if position in known:
+        return
+
+    if grid[position] == 9:
+        known[position] = 1
+        return
+
+    for direction in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        new_position = (position[0] + direction[0], position[1] + direction[1])
+        gradient = grid[new_position] - grid[position]
+
+        if gradient != 1:
+            continue
+
+        if new_position not in known:
+            find_reachable_peaks(grid, known, new_position)
+
+        known[position] += known[new_position]
