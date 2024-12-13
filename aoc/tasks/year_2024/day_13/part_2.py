a72a4692e9c402fce7da15e7b3b3aa53384a96fd
Moritz Strenger <moritz.strenger@gmail.com>
2024-12-13 17:50:03 +0100

    Add solutions and tests for Year 2024 Day 13 parts 1 and 2
    Implement the `go` functions for both parts using Z3 to compute optimal solutions. Add corresponding test cases and input data for verification, ensuring correct behavior for example scenarios.

---
+++ aoc/tasks/year_2024/day_13/part_2.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import pathlib
+
+import z3
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    total_cost = 0
+
+    for i in range(0, len(lines), 4):
+        block = lines[i : i + 4]
+
+        ax, ay = map(int, block[0][len("Button A: X+") :].split(", Y+"))
+        bx, by = map(int, block[1][len("Button B: X+") :].split(", Y+"))
+        tx, ty = map(int, block[2][len("Prize: X=") :].split(", Y="))
+
+        total_cost += solve(ax, bx, ay, by, tx + 10000000000000, ty + 10000000000000)
+
+    return total_cost
+
+
+def solve(ax: int, bx: int, ay: int, by: int, tx: int, ty: int) -> int:
+    solver = z3.Optimize()
+
+    a = z3.Int("a")
+    b = z3.Int("b")
+    cost = z3.Int("cost")
+
+    solver.add(a * ax + b * bx == tx)
+    solver.add(a * ay + b * by == ty)
+    solver.add(a >= 0)
+    solver.add(b >= 0)
+    solver.add(cost == a * 3 + b * 1)
+    solver.minimize(cost)
+
+    if solver.check() == z3.unsat:
+        return 0
+
+    model = solver.model()
+    return model[cost].as_long()
