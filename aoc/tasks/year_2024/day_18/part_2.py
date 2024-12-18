e345713e712d6869b8c814ac1bdcf6cdcb1fdb75
Moritz Strenger <moritz.strenger@gmail.com>
2024-12-18 22:39:09 +0100

    Optimize pathfinding logic in Day 18 Part 2 solution
    Refactor to update the shortest path dynamically by checking obstacle relevance before recomputing. Simplifies logic, avoids unnecessary recalculations, and enhances performance when processing obstacles.

--- aoc/tasks/year_2024/day_18/part_2.py
+++ aoc/tasks/year_2024/day_18/part_2.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import pathlib
 
 import networkx
-from rich.progress import track
 
 from aoc.common.decorators import timeit
 from aoc.common.storage import get_data_path, get_lines
@@ -31,10 +30,17 @@ def go(path: pathlib.Path = DATA_PATH, size: int = 70) -> str:
                 if 0 <= x + dx <= size and 0 <= y + dy <= size:
                     graph.add_edge((x, y), (x + dx, y + dy))
 
-    for step, obstacle in track(enumerate(obstacles)):
+    shortest_path = networkx.shortest_path(graph, start, goal)
+
+    for step, obstacle in enumerate(obstacles):
         graph.remove_node(obstacle)
 
-        if not networkx.has_path(graph, start, goal):
+        if obstacle not in shortest_path:
+            continue
+
+        try:
+            shortest_path = networkx.shortest_path(graph, start, goal)
+        except networkx.NetworkXNoPath:
             return lines[step]
 
     raise ValueError("Something went wrong")
