79d5ea452582408421ec1c14fa175e07af4df9de
Moritz Strenger <moritz.strenger@gmail.com>
2024-12-19 11:45:40 +0100

    Add solutions and tests for Day 19, Year 2024 challenge
    Implemented parts 1 and 2 for the 2024 Day 19 Advent of Code task using recursive solutions with memoization. Added corresponding unit tests and sample data to ensure functionality.

---
+++ aoc/tasks/year_2024/day_19/part_2.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import functools
+import pathlib
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    available_patterns = frozenset(lines[0].split(", "))
+    min_pattern_len = min(map(len, available_patterns))
+    max_pattern_len = max(map(len, available_patterns))
+
+    return sum(solve(line, available_patterns, min_pattern_len, max_pattern_len) for line in lines[2:])
+
+
+@functools.cache
+def solve(search_string: str, available_patterns: frozenset[str], min_pattern_len: int, max_pattern_len: int) -> int:
+    if not search_string:
+        return 1
+
+    solve_count = 0
+    for split_len in range(min_pattern_len, min(len(search_string), max_pattern_len) + 1):
+        left, right = search_string[:split_len], search_string[split_len:]
+
+        if left in available_patterns and (count := solve(right, available_patterns, min_pattern_len, max_pattern_len)):
+            solve_count += count
+
+    return solve_count
