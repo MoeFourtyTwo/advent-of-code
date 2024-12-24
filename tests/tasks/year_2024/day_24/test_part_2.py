eac10b1891cd61b144002e7b1f617231c2e2141a
Moritz Strenger <moritz.strenger@mercedes-benz.com>
2024-12-24 19:36:45 +0100

    Remove invalid test

--- tests/tasks/year_2024/day_24/test_part_2.py
+++
@@ -1,8 +0,0 @@
-from aoc.common.storage import get_data_path
-from aoc.tasks.year_2024.day_24.part_2 import go
-
-TEST_PATH = get_data_path(__file__, "test.txt")
-
-
-def test_year_2024_day_24_part_2_go():
-    assert go(TEST_PATH) == 0
