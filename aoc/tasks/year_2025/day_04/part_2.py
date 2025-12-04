4ac38aef3e39d8d8ad3ba6711ad27d9766fa0648
Moritz Strenger <moritz.strenger@gmail.com>
2025-12-04 20:27:03 +0100

    Add Part 2 solution and tests for Day 4, Year 2025 challenge
    Implement `go` function to calculate rolls removed after convolution. Add corresponding unit tests and sample data for verification.

---
+++ aoc/tasks/year_2025/day_04/part_2.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import pathlib
+
+import numpy as np
+from scipy.signal import convolve2d
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+    data = np.array([list(line.replace(".", "0").replace("@", "1")) for line in lines], dtype=int)
+
+    kernel = np.array([[-1, -1, -1], [-1, 4, -1], [-1, -1, -1]])
+
+    removed_rolls = 0
+    while True:
+        conv = convolve2d(data, kernel, mode="same", boundary="fill", fillvalue=0)
+        result = (conv >= 1).astype(int)
+        if np.all(result == 0):
+            return removed_rolls
+
+        removed_rolls += np.sum(result)
+        data -= result
