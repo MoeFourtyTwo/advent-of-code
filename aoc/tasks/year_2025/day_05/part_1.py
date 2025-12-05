a2892a890f8cd96acaf401a1c28396e20967c1de
Moritz Strenger <moritz.strenger@gmail.com>
2025-12-05 10:45:18 +0100

    Add Part 1 solution and tests for Day 5, Year 2025 challenge
    Implement `go` function to count how many numbers match predefined ranges. Add corresponding unit tests and sample data for validation.

---
+++ aoc/tasks/year_2025/day_05/part_1.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import pathlib
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    ranges = []
+
+    it = iter(lines)
+
+    for line in it:
+        if not line:
+            break
+        lower_bound, upper_bound = map(int, line.split("-"))
+        ranges.append(range(lower_bound, upper_bound + 1))
+
+    count = 0
+
+    for line in it:
+        count += any(int(line) in r for r in ranges)
+
+    return count
