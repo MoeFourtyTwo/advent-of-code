1f69656bbb3c038d0a2d9818c5693d64a8de618e
Moritz Strenger <moritz.strenger@gmail.com>
2025-12-08 22:32:13 +0100

    Add Part 2 solution and tests for Day 8, Year 2025 challenge

---
+++ aoc/tasks/year_2025/day_08/part_2.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import pathlib
+
+import networkx
+import numpy as np
+
+from aoc.common.decorators import timeit
+from aoc.common.storage import get_data_path, get_lines
+
+DATA_PATH = get_data_path(__file__)
+
+
+@timeit
+def go(path: pathlib.Path = DATA_PATH) -> int:
+    lines = get_lines(path)
+
+    points = np.array([list(map(int, row.split(","))) for row in lines], dtype=np.int64)
+    n = points.shape[0]
+
+    sq = np.sum(points * points, axis=1)
+    dist_sq = sq[:, None] + sq[None, :] - 2 * (points @ points.T)
+
+    triu_i, triu_j = np.triu_indices(n, k=1)
+    flat = dist_sq[triu_i, triu_j]
+
+    idx = np.argsort(flat)
+    i_pairs = triu_i[idx]
+    j_pairs = triu_j[idx]
+
+    graph = networkx.Graph()
+    for i in range(n):
+        graph.add_node(i)
+
+    for i, j in zip(i_pairs, j_pairs):
+        graph.add_edge(i, j)
+        if networkx.is_connected(graph):
+            return points[i, 0] * points[j, 0]
